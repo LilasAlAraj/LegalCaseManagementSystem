a34125619c8bd86efd45b7d240346978d1dc21c6
Boushra <tulipalmouhammad@gmail.com>
2023-04-24 13:50:20 +0300

    boushra

--- app/Models/Cases_details.php
+++ app/Models/Cases_details.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Cases_details extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'id_Cases',
+        'case_number',
+        'Status',
+        'decision',
+        'facts',
+        'legal_discussion',
+        'verdict'
+    ];
 }

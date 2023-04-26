757f0364fbfb0f29822640cf033270a80067ea50
Boushra <tulipalmouhammad@gmail.com>
2023-04-26 21:44:13 +0300

    boushra v_3

---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Sessions;
+use Illuminate\Http\Request;
+
+class SessionController extends Controller
+{
+    public function index()
+    {
+        $session=Sessions::all();
+
+        return view('session.index',compact( $session));
+    }
+
+    public function store(Request $request)
+
+    {
+         
+        $request->validate([
+            'date' => 'required',
+            'description' => 'required',
+            'delay_date'=>'required',
+            'delay_reasons'=>'required',
+        ],[
+
+        'date.required'=>'please fill the session date',
+        'description.required'=>'please fill the description',
+        'delay_date.required'=>'please fill the delay_date',
+        'delay_reasons.required'=>'please fill the delay_reasons',
+          
+        ]);
+
+        Sessions::create([
+            
+            'case_id' => $request->case_id,
+            'date' => $request->date,
+            'description' => $request->description,
+            'delay_date' => $request->delay_date,
+            'delay_reasons'=>$request->delay_reasons,
+        ]);
+
+        session()->flash('Add', 'تم اضافة الجلسة بنجاح ');
+
+        return redirect('/sessions');
+
+    }
+
+    public function update(Request $request)
+    {
+
+       $id = $request->id;
+
+       $this -> validate($request,[
+        'date' => 'required',
+        'description' => 'required',
+        'delay_date'=>'required',
+        'delay_reasons'=>'required',
+    ],[
+
+    'date.required'=>'please fill the session date',
+    'description.required'=>'please fill the description',
+    'delay_date.required'=>'please fill the delay_date',
+    'delay_reasons.required'=>'please fill the delay_reasons',
+      
+    ]);
+
+       $sections=Sessions::find($id);
+
+       $sections->update([
+        'date' => $request->date,
+        'description' => $request->description,
+        'delay_date' => $request->delay_date,
+        'delay_reasons'=>$request->delay_reasons,
+    ]);
+
+    session()->flash('edit','تم نعديل الجلسة بنجاح');
+    return redirect('/sections');
+
+    }
+
+    public function destroy(Request $request)
+    {
+    
+        $id=$request->id;
+
+        Sessions::find($id)->delete();
+
+        session()->flash('delete','You delete the Sessions');
+
+        return redirect('/Sessions');
+
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -18,33 +18,41 @@ Route::get('/', function () {
     return view('/home');
 });
 
-Auth::routes();
+    Auth::routes();
 
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
     
-    Route::resource('cases','App\Http\Controllers\CasesController');
+    Route::resource('cases','App\Http\Controllers\CasesController');  //القضية
 
-    Route::resource('CasesAttachments', 'CasesAttachmentController');
+    Route::resource('CasesAttachments', 'App\Http\Controllers\CasesAttachmentController'); // مرفقات القضية
+     
+    Route::resource('sessions','App\Http\Controllers\SessionsController');   // جلسات القضية
+  
+    Route::get('/CasesDetails/{id}' ,'App\Http\Controllers\CasesDetailsController@edit');  // تفاصيل القضية
 
-    Route::get('/CasesDetails/{id}' ,'App\Http\Controllers\CasesDetailsController@edit');
+    Route::get('/Status_show/{id}', 'CasesController@show')->name('Status_show');        // حالة القضية (رابحة -خاسرة -جاري العمل عليها - مفتوحة )
 
-    Route::get('/Status_show/{id}', 'CasesController@show')->name('Status_show');
+    Route::post('/Status_Update/{id}', 'CasesController@Status_Update')->name('Status_Update');  // تغيير حالة القضية
 
-    Route::post('/Status_Update/{id}', 'CasesController@Status_Update')->name('Status_Update');
+    Route::resource('Archive', 'InvoiceAchiveController');   //القضايا المؤرشفة
 
-    Route::resource('Archive', 'InvoiceAchiveController');
+    Route::get('Case_Winning','CasesController@Case_Winning'); // القضايا الرابحة 
 
-    Route::get('Case_Winning','CasesController@Case_Winning');
+    Route::get('Case_Lost','CasesController@Case_Lost');      // القضايا الخاسرة
 
-    Route::get('Case_Lost','CasesController@Case_Lost');
+    Route::get('Case_Partial','CasesController@Case_Partial');  // قضايا جاري العمل عليها
 
-    Route::get('Case_Partial','CasesController@Case_Partial');
+    Route::get('cases_report','App\Http\Controllers\Cases_ReportController@index'); // نتائج البحث
 
-    Route::get('cases_report','App\Http\Controllers\Cases_ReportController@index');
+    Route::post('Search_cases', 'App\Http\Controllers\Cases_ReportController@Search_cases'); // بحث عن قضية حسب مفتاح معين 
 
-    Route::post('Search_cases', 'App\Http\Controllers\Cases_ReportController@Search_cases');
+    Route::get('MarkAsRead_all','App\Http\Controllers\CasesController@MarkAsRead_all')->name('MarkAsRead_all'); // جعل جميع القضايا في الاشعارات مقروءة 
 
-    Route::group(['middleware' => ['auth']], function() {
+    Route::get('unreadNotifications_count', 'App\Http\Controllers\CasesController@unreadNotifications_count')->name('unreadNotifications_count'); //عدد الاشعارات الغير المقروءه 
+
+    Route::get('unreadNotifications', 'App\Http\Controllers\CasesController@unreadNotifications')->name('unreadNotifications'); // الرسائل الغير مقروءة
+
+    Route::group(['middleware' => ['auth']], function() { 
 
     Route::resource('roles','RoleController');
 
